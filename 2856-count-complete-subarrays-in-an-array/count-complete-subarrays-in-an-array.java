5c0b176a6827def1d34dcd4d9b123883653ff6e3
Vignesh <vicktvicky101@gmail.com>
2026-06-27 16:59:56 +0530

    Time: 7 ms (93.85%) | Memory: 47 MB (49.65%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,23 +1,27 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        int res = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) set.add(x);
 
-        List<Integer> st = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!st.contains(nums[i])) {
-                st.add(nums[i]);
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int j = i; j < nums.length; j++) {
-                map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-                if (map.size() == st.size()) {
-                    res++;
+        int distinct = set.size();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int left = 0, ans = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            while (map.size() == distinct) {
+                ans += nums.length - right;
+
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
                 }
+                left++;
             }
         }
 
-        return res;
+        return ans;
     }
 }

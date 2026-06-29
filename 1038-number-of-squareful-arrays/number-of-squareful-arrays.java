1a2e7cc5a95f548e5ca2cd8c868a8ae11c309357
Vignesh <vicktvicky101@gmail.com>
2026-06-29 10:40:38 +0530

    Time: 3 ms (23.94%) | Memory: 43.2 MB (23.47%) - LeetSync

--- 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
@@ -1,25 +1,24 @@
 class Solution {
     int ans = 0;
-    public int numSquarefulPerms(int[] nums) {
+    public int numSquarefulPerms(int[] nums){
         Arrays.sort(nums);
-        boolean[] used = new boolean[nums.length];
-        backtrack(nums, used, new ArrayList<>());
+        boolean[] used=new boolean[nums.length];
+        backtrack(nums,used,new ArrayList<>());
         return ans;
     }
-    public void backtrack(int[] nums, boolean[] used, List<Integer> path) {
-        if (path.size() == nums.length) {
+    public void backtrack(int[] nums,boolean[] used,List<Integer>path){
+        if (path.size()==nums.length){
             ans++;
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i])
+        for (int i=0;i<nums.length;i++){
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1])
                 continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
-            if (!path.isEmpty()) {
-                int sum = path.get(path.size() - 1) + nums[i];
-                int root = (int) Math.sqrt(sum);
-                if (root * root != sum)
+            if (!path.isEmpty()){
+                int sum=path.get(path.size()-1)+nums[i];
+                int root=(int)Math.sqrt(sum);
+                if (root*root!=sum)
                     continue;
             }
             used[i] = true;

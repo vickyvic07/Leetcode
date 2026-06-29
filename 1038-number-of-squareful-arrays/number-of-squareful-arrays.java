7c53e3536fd51fe496cba30fe641ecc1911eeda0
Vignesh <vicktvicky101@gmail.com>
2026-06-29 10:34:11 +0530

    Time: 3 ms (23.94%) | Memory: 42.9 MB (65.26%) - LeetSync

---
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
@@ -0,0 +1,32 @@
+class Solution {
+    int ans = 0;
+    public int numSquarefulPerms(int[] nums) {
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, new ArrayList<>());
+        return ans;
+    }
+    public void backtrack(int[] nums, boolean[] used, List<Integer> path) {
+        if (path.size() == nums.length) {
+            ans++;
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+            if (!path.isEmpty()) {
+                int sum = path.get(path.size() - 1) + nums[i];
+                int root = (int) Math.sqrt(sum);
+                if (root * root != sum)
+                    continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backtrack(nums, used, path);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}
